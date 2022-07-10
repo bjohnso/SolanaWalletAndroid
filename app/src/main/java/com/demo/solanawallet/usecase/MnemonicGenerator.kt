7f335bcaff34da8580a4ed51b2911d6c10bb1ac2
bjohnso <bjohnso@student.wethinkcode.co.za>
2022-07-10 19:45:35 +0200

    Mnemonic generator cleanup

--- app/src/main/java/com/demo/solanawallet/usecase/MnemonicGenerator.kt
+++ app/src/main/java/com/demo/solanawallet/usecase/MnemonicGenerator.kt
@@ -8,10 +8,12 @@ import java.util.*
 
 object MnemonicGenerator {
     operator fun invoke(context: Context, seed: ByteArray): List<String> {
+        val mnemonicWordList = context.resources.getStringArray(R.array.mnemonic_word_list)
+
         val messageDigest = MessageDigest.getInstance("SHA-256")
         val hashedSeed = messageDigest.digest(seed)
 
-        val checksumLength = ((seed.size * 8) / 32) / 8
+        val checksumLength = seed.size / 32
         val checksum = hashedSeed.take(checksumLength)
 
         val mnemonicBytes = seed + checksum
@@ -25,13 +27,11 @@ object MnemonicGenerator {
             }
         }
 
-        val mnemonicWordList = context.resources.getStringArray(R.array.mnemonic_word_list)
-        val mnemonicSentenceBinaryList = mnemonicBinaryString.chunked(11)
-
-        val mnemonicSentenceDecimalList = mnemonicSentenceBinaryList
-            .map { BigInteger(it, 2).toInt() }
-
-        return mnemonicSentenceDecimalList
-            .map { mnemonicWordList[it] }
+        return mnemonicBinaryString
+            .chunked(11)
+            .map {
+                val index = BigInteger(it, 2).toInt()
+                mnemonicWordList[index]
+            }
     }
 }
